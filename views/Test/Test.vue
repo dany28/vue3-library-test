754ca58bc329dc5987c19ff3ce338f942202f235
Dany <daniel@jursza.com>
2022-06-27 18:57:15 +0200

    Create Test.vue

---
+++ views/Test/Test.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>{{ message }}</h1>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+
+export default defineComponent({
+    setup() {
+        let message = ref('ok')
+        return {
+            message
+        }
+    }
+})
+
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
