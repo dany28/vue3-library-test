3f8d2cf2eef5d0257280e163950aaa36a60818d2
Dany <daniel@jursza.com>
2022-06-27 19:01:07 +0200

    Create Test.js

---
+++ dist/Test.js
@@ -0,0 +1,2 @@
+var Test=function(e){"use strict";var p=(()=>`h1{color:red}
+`)(),a=(t,n)=>{const r=t.__vccOpts||t;for(const[s,o]of n)r[s]=o;return r};const c=e.defineComponent({setup(){return{message:e.ref("ok")}}});function _(t,n,r,s,o,i){return e.openBlock(),e.createElementBlock("h1",null,e.toDisplayString(t.message),1)}var l=a(c,[["render",_]]);return l}(Vue);
